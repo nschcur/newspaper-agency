237b737882a3b2cb6c0e4fdd0a4fd41af5c2a5ff
Nazar <nazae530@gmail.com>
2023-12-08 21:15:59 +0200

    add installed apps - topic

--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def str(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+
+    def str(self):
+        return self.username
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publish_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor)
+
+    def str(self):
+        return self.title
