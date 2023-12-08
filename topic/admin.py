55245f22699bf607304d0913de25b1066d0d27d3
Nazar <nazae530@gmail.com>
2023-12-08 21:17:37 +0200

    create models and admins

--- topic/admin.py
+++ topic/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Topic, Redactor, Newspaper
 
-# Register your models here.
+
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'topic')
+    list_filter = ('publish_date', 'topic')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Topic)
+admin.site.register(Redactor)
+admin.site.register(Newspaper, NewspaperAdmin)
