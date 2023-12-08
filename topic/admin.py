48c1397929d88ca530af4a689afaa1b7896f2a4b
Nazar <nazae530@gmail.com>
2023-12-08 21:45:01 +0200

    edited admin

--- topic/admin.py
+++ topic/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Topic, Redactor, Newspaper
 
 
+@admin.register(Newspaper)
 class NewspaperAdmin(admin.ModelAdmin):
     list_display = ('title', 'publish_date', 'topic')
     list_filter = ('publish_date', 'topic')
     search_fields = ('title', 'content')
 
 
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+
+
 admin.site.register(Topic)
-admin.site.register(Redactor)
-admin.site.register(Newspaper, NewspaperAdmin)
