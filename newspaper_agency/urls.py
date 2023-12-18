e74b2e06ab39c35f6c098fed3c7326d859627c39
nschcur <nazae530@gmail.com>
2023-12-18 21:38:58 +0200

    editing newspaper_agency/urls

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("index/", include("topic.urls", namespace="topic"))
-]
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("topic.urls")),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
