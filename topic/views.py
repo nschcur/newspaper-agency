b0368a3abc67bb1be15393529b9eb8c486ff3b7f
nschcur <nazae530@gmail.com>
2023-12-18 21:30:31 +0200

    created all views and corrections index

--- topic/views.py
+++ topic/views.py
@@ -1,17 +1,184 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-from topic.models import Redactor, Topic, Newspaper
+from newspaper.models import Redactor, Topic, Newspaper
+from newspaper.forms import (
+    LoginForm,
+    SignUpForm,
+    RedactorCreationForm,
+    NewspaperForm,
+    NewspaperSearchForm, RedactorUpdateForm
+)
 
 
-def index(request):
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
+
+def register_user(request):
+    msg = None
+    success = False
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            msg = 'Account created successfully.'
+            success = True
+
+        else:
+            msg = 'Form is not valid'
+    else:
+        form = SignUpForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form, "msg": msg, "success": success}
+    )
+
+
+@login_required(login_url='/login/')
+def index(request: HttpRequest) -> HttpResponse:
+
     num_redactors = Redactor.objects.count()
-    num_newspapers = Newspaper.objects.count()
     num_topics = Topic.objects.count()
+    num_newspaper = Newspaper.objects.count()
+
+    list_topics = Topic.objects.all()
+    list_redactors = Redactor.objects.all()
+    list_newspaper = Newspaper.objects.all()
 
     context = {
         "num_redactors": num_redactors,
-        "num_newspapers": num_newspapers,
-        "num_topic": num_topics,
+        "num_topics": num_topics,
+        "num_newspaper": num_newspaper,
+        "list_topics": list_topics,
+        "list_redactors": list_redactors,
+        "list_newspaper": list_newspaper,
     }
 
-    return render(request, "topic/index.html", context)
+    return render(request, "home/index.html", context=context)
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 5
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+    queryset = Redactor.objects.prefetch_related(
+        "newspaper_redactors__publishers"
+    )
+
+
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Redactor
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Redactor
+    form_class = RedactorUpdateForm
+    success_url = reverse_lazy("newspaper:index")
+
+
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Redactor
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    paginate_by = 5
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Topic
+    queryset = Topic.objects.all()
+    paginate_by = 5
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NewspaperListView, self).get_context_data(**kwargs)
+        title = self.request.GET.get("title", "")
+        context["search_form"] = NewspaperSearchForm(
+            initial={"title": title}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Newspaper.objects.all()
+        form = NewspaperSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(title__icontains=form.cleaned_data["title"])
+        return queryset
+
+
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Newspaper
+
+
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class NewspaperUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class NewspaperDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Newspaper
+    success_url = reverse_lazy("newspaper:newspaper-list")
