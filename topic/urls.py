8895bb4a101eece7f8c646b8beb01ad7ee7a4ac5
nschcur <nazae530@gmail.com>
2023-12-18 21:36:24 +0200

    created urlpatterns

--- topic/urls.py
+++ topic/urls.py
@@ -1,11 +1,90 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from topic import views
-from topic.views import index
-
+from topic.views import index, login_view, register_user, RedactorListView, RedactorDetailView, RedactorCreateView, \
+    RedactorUpdateView, RedactorDeleteView, TopicListView, TopicCreateView, TopicUpdateView, TopicDeleteView, \
+    NewspaperDetailView, TopicDetailView, NewspaperListView, NewspaperCreateView, NewspaperUpdateView, \
+    NewspaperDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path(
+        "redactors/<int:pk>/",
+        RedactorDetailView.as_view(),
+        name="redactor-detail"
+    ),
+    path(
+        "redactors/create/",
+        RedactorCreateView.as_view(),
+        name="redactor-create"
+    ),
+
+    path(
+        "redactors/<int:pk>/update/",
+        RedactorUpdateView.as_view(),
+        name="redactor-update"
+    ),
+
+    path(
+        "redactors/<int:pk>/delete/",
+        RedactorDeleteView.as_view(),
+        name="redactor-delete"
+    ),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list",
+    ),
+    path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create",
+    ),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update",
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete",
+    ),
+    path(
+        "topics/<int:pk>/",
+        TopicDetailView.as_view(),
+        name="topic-detail"
+    ),
+    path(
+        "newspapers/<int:pk>/",
+        NewspaperDetailView.as_view(),
+        name="newspaper-detail"
+    ),
+    path(
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list",
+    ),
+    path(
+        "newspapers/create/",
+        NewspaperCreateView.as_view(),
+        name="newspaper-create",
+    ),
+    path(
+        "newspapers/<int:pk>/update/",
+        NewspaperUpdateView.as_view(),
+        name="newspaper-update",
+    ),
+    path(
+        "newspapers/<int:pk>/delete/",
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete",
+    ),
 ]
 
-app_name = "topic"
+app_name = "newspaper_agency"
